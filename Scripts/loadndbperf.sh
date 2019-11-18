41d6ec984a935c90c17e1fd5798b60b8ac1d2957
nieyuanyuan <nieyuanyuan@bytedance.com>
2019-11-18 17:48:09 +0800

    minor updates

--- Scripts/loadndbperf.sh
+++ Scripts/loadndbperf.sh
@@ -1,7 +1,7 @@
 NDB_PERF_BASEDIR=$HOME/mysql
 NDB_PERF_CONFIG=$HOME/ndb.cnf
-NDB_PERF_DEFAULT_TABLE_COUNT=250
-NDB_PERF_DEFAULT_TABLE_SIZE=25000
+NDB_PERF_DEFAULT_TABLE_COUNT=64
+NDB_PERF_DEFAULT_TABLE_SIZE=10000000
 NDB_PERF_DEFAULT_PASSWORD=TAKE0one
 
 function ndbperfstart()
@@ -137,7 +137,7 @@ function ndbperfinit()
   echo "innodb_io_capacity=6000" >> $NDB_PERF_CONFIG
   echo "innodb_io_capacity_max=10000" >> $NDB_PERF_CONFIG
   echo "innodb_file_per_table=1" >> $NDB_PERF_CONFIG
-  echo "innodb_buffer_pool_size=64G" >> $NDB_PERF_CONFIG
+  echo "innodb_buffer_pool_size=128G" >> $NDB_PERF_CONFIG
   echo "loose-innodb_mock_server_host=localhost:8080" >> $NDB_PERF_CONFIG
   echo "thread_handling=pool-of-threads" >> $NDB_PERF_CONFIG
   echo "thread_pool_size=64" >> $NDB_PERF_CONFIG
