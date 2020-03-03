3255358dd6be038fca13c8bc14686b48700a42f3
nieyuanyuan <nieyuanyuan@bytedance.com>
2020-03-03 16:56:42 +0800

    minor updates

--- Scripts/loadndbperf.sh
+++ Scripts/loadndbperf.sh
@@ -164,7 +164,7 @@ function ndbperfinit()
   echo "innodb_buffer_pool_size=128G" >> $NDB_PERF_CONFIG
   echo "innodb_log_writer_group_commit_timeout=100" >> $NDB_PERF_CONFIG
   echo "innodb_log_writer_group_commit_len=4K" >> $NDB_PERF_CONFIG
-  echo "loose-innodb_mock_server_host=localhost:8080" >> $NDB_PERF_CONFIG
+  echo "loose-mock_server_host=localhost:8080" >> $NDB_PERF_CONFIG
   echo "thread_handling=pool-of-threads" >> $NDB_PERF_CONFIG
   echo "thread_pool_size=64" >> $NDB_PERF_CONFIG
   echo "thread_pool_stall_limit=10" >> $NDB_PERF_CONFIG
